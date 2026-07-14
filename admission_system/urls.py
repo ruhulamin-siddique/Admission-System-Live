5ead7f62c3df8023e43feb0da7f184b84d19156a
ruhulamin-siddique <ruhulamin.baust@gmail.com>
2026-07-14 19:09:47 +0600

    feat: add register and login authentication templates and configure route definitions

--- admission_system/urls.py
+++ admission_system/urls.py
@@ -1,20 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.views.decorators.cache import never_cache
 from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     
     # Root Level Authentication
-    path('login/', auth_views.LoginView.as_view(template_name='core/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='core/password_change.html'), name='password_change'),
-    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='core/password_change_done.html'), name='password_change_done'),
+    path('login/', never_cache(auth_views.LoginView.as_view(template_name='core/login.html')), name='login'),
+    path('logout/', never_cache(auth_views.LogoutView.as_view()), name='logout'),
+    path('password-change/', never_cache(auth_views.PasswordChangeView.as_view(template_name='core/password_change.html')), name='password_change'),
+    path('password-change/done/', never_cache(auth_views.PasswordChangeDoneView.as_view(template_name='core/password_change_done.html')), name='password_change_done'),
     
     # Registration & Public Status (Root URLs)
-    path('register/', core_views.register, name='register'),
-    path('check-status/', core_views.check_status, name='check_status'),
+    path('register/', never_cache(core_views.register), name='register'),
+    path('check-status/', never_cache(core_views.check_status), name='check_status'),
 
     # Application Modules
     path('', include('students.urls')),
